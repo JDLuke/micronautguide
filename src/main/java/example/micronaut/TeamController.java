9e09f572bda30bfde4b114723cf1c3b09b8043c1
J.D. Luke <jd@jdluke.com>
2024-09-07 07:56:30 -0400

    Added additional configuration based on Micronaut Guide documentation, mixingin Lombok annotations to keep code more concise.

--- src/main/java/example/micronaut/TeamController.java
+++ src/main/java/example/micronaut/TeamController.java
@@ -5,9 +5,6 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
 
-import java.util.stream.Collectors;
-
-import static java.lang.String.join;
 
 @Controller("/team")
 public class TeamController {
