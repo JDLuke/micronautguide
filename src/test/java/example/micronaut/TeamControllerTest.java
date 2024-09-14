feeca01fc6a155a9f747ccc4c42e2d045fca895f
J.D. Luke <jd@jdluke.com>
2024-09-14 10:22:04 -0400

    Test cases for TeamController
    Signed-off-by: J.D. Luke <jd@jdluke.com>

---
+++ src/test/java/example/micronaut/TeamControllerTest.java
@@ -0,0 +1,22 @@
+package example.micronaut;
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MicronautTest
+public class TeamControllerTest {
+    @Test
+    void canCreate() {
+        TeamConfiguration teamConfiguration = new TeamConfiguration();
+        teamConfiguration.playerNames = List.of("name", "name2");
+        teamConfiguration.name = "teamName";
+        TeamController teamController = new TeamController(teamConfiguration);
+
+        assertEquals("name, name2", teamController.teamPlayers());
+        assertEquals("teamName", teamController.teamInfo());
+    }
+}
